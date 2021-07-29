bae516a85a780a003ab6924974ac08478faea87a
Quan Duong <quanduong1396@gmail.com>
2021-07-29 20:09:54 +0700

    upload sql query
    upload sql query

---
+++ Data_Create.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO DANHMUC VALUES ('DMX001', 'Xe Tang', 'Các loại xe tăng chiến đấu')
+INSERT INTO DANHMUC VALUES ('DMS002' , 'Sung Truong', 'Các loại súng trường')
+
+INSERT INTO MATHANG VALUES ('MHX001', 'Xe Tang M48 Patton', 42, '', 309090, 'M48 Patton là xe tăng chiến tranh của Mỹ.', 'DMX001')
+INSERT INTO MATHANG VALUES ('MHX002', 'Xe Tang Challenger 1', 27, '', 1500000, 'Challenger 1 là xe tăng chiến đấu do Anh sản xuất', 'DMX001')
+
+INSERT INTO TAIKHOAN VALUES ('baesuzy1993', 'Bae Suzy', 'suzycute@gmail.com', '0937060413', 'Seoul', 1, 'Han Quoc', 'meandher1312', 1)
+INSERT INTO TAIKHOAN VALUES ('kimjiwon1992', 'Kim Ji Won', 'jiwoncute@gmail.com', '0937060423', 'Seoul', 1, 'Han Quoc', 'meandher1312', 0)
+
+DROP TABLE Customers
